9c5140df5fded5e69508cd11a2f17213e6413e80
nagu52 <knagarajsagi@gmail.com>
2014-09-27 20:24:15 -0400

    Update bootstrap.js
    Test changes

--- angularjs/js/bootstrap.js
+++ angularjs/js/bootstrap.js
@@ -1,6 +1,7 @@
 /*!
  * Bootstrap v3.2.0 (http://getbootstrap.com)
  * Copyright 2011-2014 Twitter, Inc.
+ * Change done for Testing
  * Licensed under MIT (https://github.com/twbs/bootstrap/blob/master/LICENSE)
  */
 
