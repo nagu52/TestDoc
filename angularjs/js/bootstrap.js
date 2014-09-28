6cff1af26bf7be75cd376baee391b33fe2cd19bd
nagu52 <knagarajsagi@gmail.com>
2014-09-27 20:22:49 -0400

    Update bootstrap.js

--- angularjs/js/bootstrap.js
+++ angularjs/js/bootstrap.js
@@ -1,6 +1,7 @@
 /*!
  * Bootstrap v3.2.0 (http://getbootstrap.com)
  * Copyright 2011-2014 Twitter, Inc.
+ * Test Changes by Me
  * Licensed under MIT (https://github.com/twbs/bootstrap/blob/master/LICENSE)
  */
 
